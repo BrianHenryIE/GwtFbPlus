342d72488260716297b8aec0bcf87882c7b899be
Brian Henry <brian.henry2@gmail.com>
2014-03-03 02:06:19 +0000

    Now accepts page_id GET parameter so page tabs can work outside Facebook.

--- src/ie/sortons/gwtfbplus/server/LandingPageServlet.java
+++ src/ie/sortons/gwtfbplus/server/LandingPageServlet.java
@@ -61,7 +61,7 @@ public abstract class LandingPageServlet extends HttpServlet {
 		if (request.getParameter("signed_request") != null) {
 
 			signedRequest = SignedRequest.parseSignedRequest(request.getParameter("signed_request"));
-
+			
 			String encryptedSignedRequest = null;
 			SimpleStringCipher ssc = new SimpleStringCipher(appSecret);
 			try {
@@ -88,36 +88,12 @@ public abstract class LandingPageServlet extends HttpServlet {
 			}
 
 			signedRequestData = "  <script id=\"signedRequest\">\n" + "    var _sr_data = " + gson.toJson(signedRequest) + "\n  </script>\n\n";
+		
 		}
 		// This isn't needed/desirable outside the fb canvas
 		overflow = " style=\"overflow: hidden\"";
 
-		// If the app has been added as a Page tab, it redirects to the canvas with a URL:
-		// http://apps.facebook.com/sortonsdev/?tabs_added[356718097671739]=1#_=_
-		// or if it's added to multiple pages:
-		// http://apps.facebook.com/sortonsdev/?tabs_added[367864846557326]=1&tabs_added[356718097671739]=1#_=_
-		// Regex for extracting the url from wall posts
-
-		if (request.getHeader("referer").matches(".*tabs_added%5B(\\d+)%5D.*")) {
-
-			// If the Canvas load was a redirect from adding the page tab
-			// redirect to the page that added it, at the app's new tab
-			// TODO Test this... it didn't seem to work once, though the URL worked when tested in isolation
-			// TODO maybe just // instead of a method there.
-			String redirectTo = httpOrS(request) + "www.facebook.com/" + request.getHeader("referer").replaceAll(".*tabs_added%5B(\\d+)%5D.*", "$1")
-					+ "?sk=app_" + appId;
-
-			PrintWriter out = response.getWriter();
-			out.print("<script>window.top.location.href = \"" + redirectTo + "\";</script>");
-
-			// TODO
-			// Put some text here in case the redirect doesn't work (why wouldn't it?)
-			// If multiple pages have had the app added, show a list rather than redirect.
-			// Do it with GWT? The app will probably be cached already
-
-		} else {
-			doGet(request, response);
-		}
+		doGet(request, response);
 
 		// TODO
 		// The "Go to application" link in the admin section of a Page links to
@@ -130,34 +106,82 @@ public abstract class LandingPageServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		// TODO
+		// out.write should be its own thing, not post calling get
+		
 		System.out.println("Servlet execution... " + request.getMethod());
 
-		this.request = request;
-		this.response = response;
-		readWriteRequest();
+		if(request.getParameter("page_id")!=null){
+						 
+			signedRequestData = "  <script id=\"signedRequest\">\n" + "    var _sr_data = {\"page\": { \"id\": \""+request.getParameter("page_id")+"\", \"admin\": false}, \"user_id\": \"\" }\n  </script>\n\n";
 
-		PrintWriter out = response.getWriter();
+		}
+		
+		// If the app has been added as a Page tab, it redirects to the canvas with a URL:
+		// http://apps.facebook.com/sortonsdev/?tabs_added[356718097671739]=1#_=_
+		// or if it's added to multiple pages:
+		// http://apps.facebook.com/sortonsdev/?tabs_added[367864846557326]=1&tabs_added[356718097671739]=1#_=_
+		// Regex for extracting the url from wall posts
 
-		// Write out head
-		out.print("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"> \n"
-				+ // As specified for Bing Maps API
-				"<html xmlns=\"http://www.w3.org/1999/xhtml\"" + overflow + "> \n\n" + "<head> \n\n"
-				+ "  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/> \n\n"
-				+ signedRequestData
-				+ // If available, print the Signed Request
-				"  <meta name=\"gwt:property\" content=\"" + request.getLocale() + "\">\n\n"
-				+ "  <script type=\"text/javascript\" language=\"javascript\" src=\"" + gwtEntryPoint + "\"></script> \n\n"
-				+ "  <script src=\"//connect.facebook.net/en_US/all.js\"></script> \n\n" + // Facebook API
-				head + "</head> \n\n");
+		// TODO check get parameters :
+		// http://sortonsevents.appspot.com/recentposts/?tabs_added%5B218374251693428%5D=1#_=_
+		
+		if (request.getHeader("referer") != null && request.getHeader("referer").contains("tabs_added")) {
 
-		// Write out body
-		out.print("<body" + overflow + "> \n\n" + "  <div id='fb-root'></div> \n\n" + // required for Facebook API
-				"  <div id=\"gwt\"></div> \n\n" + // root of document for GWT
-				body + "</body> \n\n");
+			// If the Canvas load was a redirect from adding the page tab
+			// redirect to the page that added it, at the app's new tab
+			// TODO Test this... it didn't seem to work once, though the URL worked when tested in isolation
+			// TODO maybe just // instead of a method there.
+			// TODO This will only work when adding to one page at a time!
+			String redirectTo = httpOrS(request) + "www.facebook.com/" + request.getHeader("referer").replaceAll(".*tabs_added%5B(\\d+)%5D.*", "$1")
+					+ "?sk=app_" + appId;
 
-		out.print("</html>");
-		out.flush();
+			PrintWriter out = response.getWriter();
+			out.print("<script>window.top.location.href = \"" + redirectTo + "\";</script>");
 
+			// TODO
+			// Put some text here in case the redirect doesn't work (why wouldn't it?)
+			// If multiple pages have had the app added, show a list rather than redirect.
+			// Do it with GWT? The app will probably be cached already
+
+		} else {
+
+			this.request = request;
+			this.response = response;
+			readWriteRequest();
+
+			PrintWriter out = response.getWriter();
+
+			// Write out head
+			out.print("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"> \n"
+					+ // As specified for Bing Maps API
+					"<html xmlns=\"http://www.w3.org/1999/xhtml\""
+					+ overflow
+					+ "> \n\n"
+					+ "<head> \n\n"
+					+ "  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/> \n\n"
+					+ signedRequestData
+					+ // If available, print the Signed Request
+					"  <meta name=\"gwt:property\" content=\""
+					+ request.getLocale()
+					+ "\">\n\n"
+					+ "  <script type=\"text/javascript\" language=\"javascript\" src=\""
+					+ gwtEntryPoint
+					+ "\"></script> \n\n"
+					+ "  <script src=\"//connect.facebook.net/en_US/all.js\"></script> \n\n" + // Facebook API
+					head + "</head> \n\n");
+
+			// Write out body
+			out.print("<body" + overflow + "> \n\n" + "  <div id='fb-root'></div> \n\n" + // required for Facebook API
+					"  <div id=\"gwt\"></div> \n\n" + // root of document for GWT
+					body + "</body> \n\n");
+
+			out.print("</html>");
+			out.flush();
+
+		}
+
+		signedRequestData = "";
+		overflow = "";
 	}
-
 }
